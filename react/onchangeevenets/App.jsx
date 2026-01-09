a374f08f37e58fb029b6812ca700bb90bf30fe3e
Siham Ouchane <souchane@e3r10p17.1337.ma>
2026-01-09 16:19:03 +0100

    react onchange input select texterea

---
+++ react/onchangeevenets/App.jsx
@@ -0,0 +1,18 @@
+// import Student from "./Student.jsx";
+// import  from "./Student.jsx";
+// import UserGreeting from "./UserGreeting";
+// import List1 from "./List1";
+// import PropTypes from 'prop-types';
+// import List3 from "./List3"
+// import Button from "./Button"
+// import ProfilePicture from "./ProfilePicture.jsx"
+// import UseSt from "./UseSt";
+// import Counter from "./Counter";
+import OnChange from "./OnChange";
+function App() {
+  return (
+    <OnChange></OnChange>
+  );
+}
+export default App
+// app is the parent and studnet is teh child
